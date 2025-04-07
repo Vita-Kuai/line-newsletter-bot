fcdd78643a65090c9faaa6b6f028d33d93629a01
Vita-Kuai <vita901221be@gmail.com>
2025-04-07 21:05:37 +0800

    Update app.py

--- app.py
+++ app.py
@@ -32,10 +32,7 @@ def handle_message(event):
     print(f"🧸 使用者 ID：{user_id}")  # 這行會印出 userId 到 Render 的 log
     reply = TextSendMessage(text='你說了：' + text)
     line_bot_api.reply_message(event.reply_token, reply)
-def send_newsletter():
-    user_id = '<<在這裡貼上你的 LINE user ID>>'
-    message = TextSendMessage(text='📰 這是你的最新電子報：今日大肥語錄：開口就輸了😌')
-    line_bot_api.push_message(user_id, message)
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=10000)
